a1222f17c4e83ee70a95ec121a1d3555c8849692
Sungwoo Choo <choo@sungwoo.me>
2016-06-11 22:35:00 +0200

    Initial Commit

---
+++ GdanskBus/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GdanskBus
+//
+//  Created by Sungwoo on 6/11/16.
+//  Copyright © 2016 Kevin Studio. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
